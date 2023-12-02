a3e914b8f3daa49f4571ef9593b471e106610cbc
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-02 23:49:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import time
 import multiprocessing
 from form_filler import fill_form
+from register import register_form
 
 if __name__ == '__main__':
-    url = "https//shop077.com/forgetPassword"
+    url_form = "https//shop077.com/forgetPassword"
+    url_register="https://shop077.com/register"
 
     # Example list of numbers
     numbers_list = [1521585344, 1234567890, 9876543210]
     processes = []
     for number in numbers_list:
-        p = multiprocessing.Process(target=fill_form, args=(url, number))
+        p = multiprocessing.Process(target=fill_form, args=(url_form, number))
         p.start()
         processes.append(p)
-
-    # Wait for all processes to finish
     for process in processes:
         process.join()
     while True:
-        for number in numbers_list:
-            p = multiprocessing.Process(target=fill_form, args=(url, number))
+        for number in range(0,5):
+            p = multiprocessing.Process(target=registe, args=url)
             p.start()
             processes.append(p)
         time.sleep(60)
