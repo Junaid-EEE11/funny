7d69f73708057e3312f0bdd7b29ad743ef474c2e
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-03 20:17:44 +0600

    Update form_filler.py

--- form_filler.py
+++ form_filler.py
@@ -1,15 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import random
-import string
-from selenium.webdriver.firefox.options import Options
+#driver = webdriver.Firefox()
+#driver.get('https://shop077.com/forgetPassword')
 
-driver = webdriver.Firefox()
-driver.get('https://shop077.com/forgetPassword')
-
-def generate_phone_number():
-    return '1' + ''.join(random.choices(string.digits, k=9));
+#def generate_phone_number():
+ #   return '1' + ''.join(random.choices(string.digits, k=9));
 
 def fill_form(url, number):
     options = Options()
@@ -17,7 +10,7 @@ def fill_form(url, number):
     driver = webdriver.Firefox(options=options)
     driver.get(url)
     phone_number_input = driver.find_element(By.CSS_SELECTOR, 'div.phone input[placeholder="Phone number"]')
-    phone_number_input.send_keys(generate_phone_number())
+    phone_number_input.send_keys(number) #generate_phone_number()
     send_button = driver.find_element(By.CSS_SELECTOR, 'div.payment button')
     send_button.click()
     time.sleep(60)
