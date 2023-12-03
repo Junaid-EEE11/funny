cb0946e732892a75f2573f33e3ae3e4ccb06b314
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-03 12:56:06 +0600

    Update form_filler.py

--- form_filler.py
+++ form_filler.py
@@ -7,10 +7,12 @@ from selenium.webdriver.firefox.options import Options
 
 driver = webdriver.Firefox()
 driver.get('https://shop077.com/forgetPassword')
+
 def generate_phone_number():
-    #return '1' + ''.join(random.choices(string.digits, k=9))
+    return '1' + ''.join(random.choices(string.digits, k=9));
+
 def fill_form(url, number):
-    options =Options()
+    options = Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     driver.get(url)
@@ -18,5 +20,5 @@ def fill_form(url, number):
     phone_number_input.send_keys(generate_phone_number())
     send_button = driver.find_element(By.CSS_SELECTOR, 'div.payment button')
     send_button.click()
-    time.sleep(60)  # Wait for the action to complete (adjust as needed)
+    time.sleep(60)
     driver.quit()
