aa934aad64dec5375c07c354d9e5ff29f9474115
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-03 12:22:21 +0600

    Update register.py

--- register.py
+++ register.py
@@ -5,30 +5,34 @@ import random
 import string
 from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
-driver.get('https://shop077.com/register?invite_code=fa49ec')
-def generate_phone_number1():
-    #return ''.join(random.choices(string.ascii_letters, k=8))+'@gmail.com'
-def generate_phone_number2():
-    #return '1' + ''.join(random.choices(string.digits, k=9))
-def generate_phone_number3():
-    #return 'T0@_' + ''.join(random.choices(string.digits, k=9))
-def generate_phone_number4():
-    #return '1' + ''.join(random.choices(string.digits, k=5))
-def fill_form(url, number):
+def generate_user_name():
+    return ''.join(random.choices(string.ascii_letters, k=8))+'@gmail.com'
+def generate_phone_number():
+    return '1' + ''.join(random.choices(string.digits, k=9))
+def generate_password():
+    return 'T0@_' + ''.join(random.choices(string.digits, k=9))
+def set_payemnt_password():
+    number_inputs = driver.find_elements(By.CSS_SELECTOR, 'div');
+    phone_number_input=[str(i.text=="Set payment password") for i in number_inputs]
+    number_input[phone_number_input.index('True')].click()
+    phone_number=driver.find_elements(By.CSS_SELECTOR, 'div.van-key')
+    [phone_number[i].click() for i in random.choice(range(0,10), k=6)]
+
+def fill_form(url):
     options =Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     driver.get(url)
     phone_number_input = driver.find_element(By.CSS_SELECTOR, 'div.phone input[placeholder="Please enter the user name"]')
-    phone_number_input.send_keys(generate_phone_number1())
+    phone_number_input.send_keys(generate_user_name())
     phone_number_input = driver.find_element(By.CSS_SELECTOR, 'div.phone input[placeholder="Please enter the phone number"]')
-    phone_number_input.send_keys(generate_phone_number2())
+    phone_number_input.send_keys(generate_phone_number())
     phone_number_input = driver.find_element(By.CSS_SELECTOR, 'div.phone input[placeholder="Enter password"]')
-    phone_number_input.send_keys(generate_phone_number3())
+    phone_number_input.send_keys(generate_password())
     phone_number_input = driver.find_element(By.CSS_SELECTOR, 'div.phone input[placeholder="Set 6 digit payment password"]')
-    phone_number_input.send_keys(generate_phone_number4())
+    phone_number_input.click();
+    set_payemnt_password()
     send_button = driver.find_element(By.CSS_SELECTOR, 'div.Register Now')
     send_button.click()
-    time.sleep(5)  # Wait for the action to complete (adjust as needed)
+    time.sleep(5)
     driver.quit()
