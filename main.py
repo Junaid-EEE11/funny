7fce3c0c5e995a7b9ca3f6e8e7192c8bb74e51af
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-03 21:24:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     url_form = "https//shop077.com/forgetPassword"
     url_register="https://shop077.com/register?invide_code=fa4e9ec"
     
-    numbers_list = [1521585344, 1713850617, 1312429829]
+    numbers_list = [1521585344, 1713850617, 1312429829, 1512429529]
     processes = []
     for number in numbers_list:
         p = multiprocessing.Process(target=fill_form, args=(url_form, number))
