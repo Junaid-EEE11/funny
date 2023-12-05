8182f7f6052f2b1b3a66ce8c5929229580837c1d
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-05 07:43:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import random
-import string
-import selenium
-from selenium.webdriver.firefox.options import Options
 import time
 import multiprocessing
 from form_filler import fill_form
