7312fb677308b83654a459a6b6fd07e95d2f460f
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-05 07:43:36 +0600

    Update form_filler.py

--- form_filler.py
+++ form_filler.py
@@ -1,9 +1,15 @@
 #driver = webdriver.Firefox()
 #driver.get('https://shop077.com/forgetPassword')
 import time
-import os
-def generate_phone_number():
-	return '1' + ''.join(random.choices(string.digits, k=9));
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import random
+import string
+import selenium
+from selenium.webdriver.firefox.options import Options
+
+#def generate_phone_number():
+	#return '1' + ''.join(random.choices(string.digits, k=9));
 def fill_form(url, number):
 	options = Options()
 	options.add_argument("--headless");
